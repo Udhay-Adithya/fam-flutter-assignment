00625f1791faa6cc35a5c3496297738d2813c9e7
Udhay-Adithya <udhayxd@gmail.com>
2024-12-28 14:06:00 +0530

    feat: add hc1 card

--- lib/features/cards/presentation/widgets/design_hc1_widget.dart
+++ lib/features/cards/presentation/widgets/design_hc1_widget.dart
@@ -13,7 +13,7 @@ class DesignHC1Widget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: SizedBox(
-        height: hcGroup.height,
+        height: hcGroup.height * 1.3,
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemCount: hcGroup.cards.length,
@@ -29,46 +29,69 @@ class DesignHC1Widget extends StatelessWidget {
   Widget _buildHc1Card(BuildContext context, card.Card card) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4.0),
-      child: SizedBox(
+      child: Container(
         height: 64,
-        width: MediaQuery.sizeOf(context).width - 16,
-        child: ListTile(
-          contentPadding: const EdgeInsets.symmetric(
-            vertical: 4,
-            horizontal: 24,
-          ),
-          tileColor: convertHexToColor(card.bgColor),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(
-              card.icon?.imageUrl ?? "",
-              fit: BoxFit.cover,
-              width: ((card.iconSize)?.toDouble() ?? 16) * 2,
-              height: (card.iconSize?.toDouble() ?? 16) * 2,
+        width: hcGroup.isScrollable
+            ? MediaQuery.sizeOf(context).width - 20
+            : (MediaQuery.sizeOf(context).width - 20) / 2,
+        decoration: BoxDecoration(
+          color: convertHexToColor(card.bgColor),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 12,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  card.icon?.imageUrl ??
+                      "https://hok.famapp.co.in/hok-assets/feedMedia/ext/5435b4ee-a962-4531-95d5-889e4038eece-1734193661283.webp",
+                  fit: BoxFit.cover,
+                  width: 48,
+                  height: 48,
+                ),
+              ),
             ),
-          ),
-          title: card.formattedTitle != null
-              ? Text(
-                  card.formattedTitle?.entities.first.text ?? "",
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              : null,
-          subtitle: card.formattedDescription != null
-              ? Text(
-                  card.formattedDescription?.entities.first.text ?? "",
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              : null,
-          trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (card.formattedTitle != null)
+                      Text(
+                        card.formattedTitle?.entities.first.text ?? "",
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        maxLines: 1,
+                      ),
+                    if (card.formattedDescription != null)
+                      Text(
+                        card.formattedDescription?.entities.first.text ?? "",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black54,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        maxLines: 2,
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
